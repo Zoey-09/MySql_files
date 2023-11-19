cd3133abf0f304fbbec638a94b659aafda0277b7
Zoey-09 <125139368+Zoey-09@users.noreply.github.com>
2023-11-19 19:15:40 +0800

    weekend_hours_worked.sql

--- weekend_hours_worked.sql
+++ weekend_hours_worked.sql
@@ -1,16 +1,16 @@
 WITH weekend_hours AS (
 	SELECT
-		emp_id,
-        TIME(timestamp) AS hour,
-        TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp)) AS lag_hour,
-        TIMEDIFF(TIME(timestamp), TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp))) AS hours_work,
-        HOUR(TIMEDIFF(TIME(timestamp), TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp)))) AS log_hours
+	    emp_id,
+	    TIME(timestamp) AS hour,
+            TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp)) AS lag_hour,
+            TIMEDIFF(TIME(timestamp), TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp))) AS hours_work,
+            HOUR(TIMEDIFF(TIME(timestamp), TIME(LAG(timestamp) OVER (PARTITION BY CAST(timestamp AS date), emp_id ORDER BY timestamp)))) AS log_hours
 	FROM attendance
-    WHERE DAYOFWEEK(timestamp) IN (7, 1)
+    	WHERE DAYOFWEEK(timestamp) IN (7, 1)
 )
     
 SELECT
-	emp_id, 
+    emp_id, 
     SUM(log_hours) AS work_hours
 FROM weekend_hours
 GROUP BY emp_id
